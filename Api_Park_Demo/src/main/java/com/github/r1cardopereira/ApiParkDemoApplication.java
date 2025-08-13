186262239723aeec7e9a3e829fd3120115e7af69
Jose Ricardo Pereira Junior <ricardo.pereira.jr@ibm.com>
2025-08-13 10:08:03 -0300

    Adicionando Projeto Springboot

---
+++ Api_Park_Demo/src/main/java/com/github/r1cardopereira/ApiParkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.r1cardopereira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiParkDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiParkDemoApplication.class, args);
+	}
+
+}
---
+++ Api_Park_Demo/src/test/java/com/github/r1cardopereira/ApiParkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.r1cardopereira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiParkDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
